370d2aa767928c25ac3ef8583350ecdd17b8b4c0
Nayeem Hossain <nayeem.cse.cou@gmail.com>
2023-07-12 22:27:44 +0600

    fix bugs

--- app/src/main/java/com/cp/contesttracker/Utility.java
+++ app/src/main/java/com/cp/contesttracker/Utility.java
@@ -2,21 +2,19 @@ package com.cp.contesttracker;
 
 import android.util.Log;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class Utility {
     public static Date parseTimeStamp(String timestamp){
 
-
-
-        SimpleDateFormat inputFormat = new SimpleDateFormat("dd.MM EEE HH:mm", Locale.ENGLISH);
-
+        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH);
+        inputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         Date outputDate = null;
 
         try {
@@ -31,6 +29,7 @@ public class Utility {
 
     public static String formatTimeStamp(Date timeStamp){
         SimpleDateFormat outputFormat = new SimpleDateFormat("dd MMMM '|' EEEE '|' hh:mm a", Locale.ENGLISH);
+        outputFormat.setTimeZone(TimeZone.getTimeZone("Asia/Dhaka"));
         return outputFormat.format(timeStamp);
     }
 
@@ -44,7 +43,7 @@ public class Utility {
 
     public static Date getCurrentDate(){
         LocalDateTime currentDate = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM EEE HH:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
         String formattedDate = currentDate.format(formatter);
         return parseTimeStamp(formattedDate);
 
